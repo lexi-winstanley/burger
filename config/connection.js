d24b9560c520efa6fe92e037a507930169d95dc2
lexi-winstanley <50344054+lexi-winstanley@users.noreply.github.com>
2019-08-28 15:52:47 -0700

    Added remote db for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'nodeuser',
-  password: 'password',
-  database: 'burgers_db'
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'nodeuser',
+    password: 'password',
+    database: 'burgers_db'
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
